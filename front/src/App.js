eb7b0a5a98d27304fa551a650c328e0dbdc66b79
ViktoriiaKrasko <akiv.co@gmail.com>
2023-09-12 18:35:06 +0300

    react-twitter

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import PostList from "../src/container/post-list";
+import Page from "../src/component/page";
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <Page>
+      <PostList />
+    </Page>
+  );
 }
 
 export default App;
